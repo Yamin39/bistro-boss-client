dc79eb6bf8f503b90e75c9decf8b1cdc0ff6105b
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-06-08 22:21:59 +0600

    added navbar

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Root = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <div className="w-11/12 max-w-screen-xl mx-auto">
         <Outlet></Outlet>
       </div>
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import { TiShoppingCart } from "react-icons/ti";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">HOME</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact-us">CONTACT US</NavLink>
+      </li>
+      <li>
+        <NavLink to="/dashboard">DASHBOARD</NavLink>
+      </li>
+      <li>
+        <NavLink to="/our-menu">OUR MENU</NavLink>
+      </li>
+      <li>
+        <NavLink to="/our-shop">OUR SHOP</NavLink>
+      </li>
+      <li className="tooltip tooltip-bottom" data-tip="Cart">
+        <NavLink to="/cart" className="btn w-full btn-circle lg:btn-sm">
+          <TiShoppingCart />
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">LOGIN</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="w-[98%] max-w-screen-xl mx-auto mt-4">
+      <div className="navbar justify-between bg-base-100">
+        <div>
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              {navLinks}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Bistro Boss</a>
+        </div>
+        <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
